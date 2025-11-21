ca496cca1d4c2c181e04e4a11e195a0c1aa2d9a5
julianperez53 <jperez53@student.cscc.edu>
2025-11-20 23:35:39 -0500

    alien stays in bounds, changes directions when hitting edges

--- settings.py
+++ settings.py
@@ -28,6 +28,7 @@ class Settings:
         self.bullet_amount = 5
 
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
-        self.fleet_speed = 2
         self.alien_w = 40
         self.alien_h = 40
+        self.fleet_speed = 2
+        self.fleet_direction = 1
