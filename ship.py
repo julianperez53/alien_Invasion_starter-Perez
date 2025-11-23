f108d242f21f9cab5cecc8c6fba2d73f6dfb9b92
julianperez53 <jperez53@student.cscc.edu>
2025-11-23 00:19:43 -0500

    set up ship to reset when colliding with aliens

--- ship.py
+++ ship.py
@@ -23,11 +23,14 @@ class Ship:
         self.image = pygame.transform.scale(self.image, (self.settings.ship_w, self.settings.ship_h))
 
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.boundaries.midbottom
+        self._center_ship()
         self.moving_right = False
         self.moving_left = False
-        self.x = float(self.rect.x)
         self.arsenal = arsenal
+
+    def _center_ship(self):
+        self.rect.midbottom = self.boundaries.midbottom
+        self.x = float(self.rect.x)
 
     def update(self) -> None:
         """updates position of ship"""
@@ -52,3 +55,9 @@ class Ship:
     def fire(self) -> bool:
         """returns bool based on if player is able to fire a bullet"""
         return self.arsenal.fire_bullet()
+    
+    def check_collisions(self, other_group):
+        if pygame.sprite.spritecollideany(self, other_group):
+            self._center_ship()
+            return True
+        return False
