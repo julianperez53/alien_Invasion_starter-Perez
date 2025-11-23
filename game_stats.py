aca9c5d147636a80ea883e5f6a815a66d3b154d6
julianperez53 <jperez53@student.cscc.edu>
2025-11-23 00:40:59 -0500

    Finished Game Status check to only play while there are ships remaining

---
+++ game_stats.py
@@ -0,0 +1,6 @@
+
+
+class GameStats():
+
+    def __init__(self, ship_limit):
+        self.ships_left = ship_limit
--- settings.py
+++ settings.py
@@ -19,6 +19,7 @@ class Settings:
         self.ship_w = 40
         self.ship_h = 60
         self.ship_speed = 5
+        self.starting_ship_count = 3
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'pixelFireball.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'fireballSound.mp3'
